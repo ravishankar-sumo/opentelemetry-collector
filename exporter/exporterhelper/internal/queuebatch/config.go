e037b81d780dc7b2b90336c6f2ef07ea671dd341
Ravishankar Gnanaprakasam <r.gnanaprakasam@sumologic.com>
2026-04-29 19:29:06 +0530

    [exporter/exporterhelper] Add cardinality_limit for the batch partitions

--- exporter/exporterhelper/internal/queuebatch/config.go
+++ exporter/exporterhelper/internal/queuebatch/config.go
@@ -118,6 +118,9 @@ type PartitionConfig struct {
 	//
 	// Entries are case-insensitive. Duplicated entries will trigger a validation error.
 	MetadataKeys []string `mapstructure:"metadata_keys"`
+
+	// Hard limit for the number of active partitions
+	CardinalityLimit int `mapstructure:"cardinality_limit"`
 }
 
 func (cfg *BatchConfig) Validate() error {
@@ -162,6 +165,10 @@ func (cfg *PartitionConfig) Validate() error {
 			return fmt.Errorf("duplicate entry in metadata_keys: %q (case-insensitive)", l)
 		}
 		uniq[l] = true
+	}
+
+	if cfg.CardinalityLimit < 0 {
+		return fmt.Errorf("`cardinality_limit` must be positive, found %d", cfg.CardinalityLimit)
 	}
 
 	return nil
